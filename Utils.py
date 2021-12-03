d5a5fa9038b5fc1b38f3d362a30aff1ae2ab3b15
Paolo Ruggirello <ruggirello99@hotmail.it>
2021-12-03 20:09:45 +0100

    introducing multiple month management

--- Utils.py
+++ Utils.py
@@ -4,12 +4,12 @@ import pandas as pd
 from datetime import datetime
 
 PAYMENTS_TYPE_DICTIONARY = {
-        1: 'Credit card',
-        2: 'Cash',
-        3: 'No charge',
-        4: 'Dispute',
-        5: 'Unknown',
-        6: 'Voided trip'
+    1: 'Credit card',
+    2: 'Cash',
+    3: 'No charge',
+    4: 'Dispute',
+    5: 'Unknown',
+    6: 'Voided trip'
 }
 
 
@@ -19,13 +19,12 @@ def initializeParser():
                         help="The year of which you wants to obtain the analysis",
                         type=str)
     parser.add_argument("-m", "--month",
-                        help="The year of which you wants to obtain the analysis. Month must be expressed as number "
+                        help="The month of which you wants to obtain the analysis. Month must be expressed as number "
                              "or in english.",
                         type=str)
     parser.add_argument("-b", "--borough",
                         help="The zone of which you wants to obtain the analysis",
-                        type=str,
-                        default='.json')
+                        type=str)
     return parser.parse_args()
 
 
@@ -47,12 +46,12 @@ def getYearFromParser(year):
         exit()
 
 
-def generateFileName(year, month):
-    return 'yellow_tripdata_' + str(year).zfill(4) + '-' + str(month).zfill(2) + '.csv'
+def generateFileNames(year, months):
+    return ['yellow_tripdata_' + str(year).zfill(4) + '-' + str(m).zfill(2) + '.csv' for m in months]
 
 
-def getMonthFromParser(month):
-    if month != None:
+def getMonthToAnalizeFromParser(month):
+    if month is not None:
         try:
             stringSize = len(month)
             if stringSize < 3:
@@ -61,18 +60,20 @@ def getMonthFromParser(month):
                 month = datetime.strptime(month, '%b').month
             else:
                 month = datetime.strptime(month, '%B').month
-            return month
+            return [month]
         except:
             print('Error! Month format not recognized.')
             exit()
-    return month
+    return list(range(1, 13))
 
 
 def readCsv(fileName):
     try:
-        yellow_taxi_tripdata = pd.read_csv('source-data/'+fileName, usecols=['payment_type', 'DOLocationID'])
-        lookup_table = pd.read_csv('source-data/taxi+_zone_lookup.csv', usecols=['LocationID', 'Borough'])  #TODO: manage possible dynamic file location and name
+        yellow_taxi_tripdata = pd.read_csv('source-data/' + fileName, usecols=['payment_type', 'DOLocationID'])
+        lookup_table = pd.read_csv('source-data/taxi+_zone_lookup.csv', usecols=['LocationID',
+                                                                                 'Borough'])  # TODO: manage possible dynamic file location and name
         return pd.merge(yellow_taxi_tripdata, lookup_table, left_on='DOLocationID', right_on='LocationID')
     except Exception as execption:
-        # print('Data-source not found for given dates.')
-        print(execption)
+        print('Data-source not found for given dates.')
+        exit()
+        # print(execption)
--- main.py
+++ main.py
@@ -1,19 +1,16 @@
-import matplotlib.pyplot as plt
-import pandas as pd
-
 import Utils
 
 if __name__ == '__main__':
     args = Utils.initializeParser()
     year = Utils.getYearFromParser(args.year)
-    month = Utils.getMonthFromParser(args.month)
-    fileName = Utils.generateFileName(year, month)
-    taxi_trip_dataframe = Utils.readCsv(fileName)
-    payments_type = taxi_trip_dataframe.groupby(['payment_type']).size()
-    most_common_pt = payments_type.idxmax()
-    less_common_pt = payments_type.idxmin()
-    print(Utils.PAYMENTS_TYPE_DICTIONARY.get(most_common_pt))
-    print(Utils.PAYMENTS_TYPE_DICTIONARY.get(less_common_pt))
+    months = Utils.getMonthToAnalizeFromParser(args.month)
+    fileNames = Utils.generateFileNames(year, months)
+    # taxi_trip_dataframe = Utils.readCsv(fileName)
+    # payments_type = taxi_trip_dataframe.groupby(['payment_type']).size()
+    # most_common_pt = payments_type.idxmax()
+    # less_common_pt = payments_type.idxmin()
+    # print(Utils.PAYMENTS_TYPE_DICTIONARY.get(most_common_pt))
+    # print(Utils.PAYMENTS_TYPE_DICTIONARY.get(less_common_pt))
     #
     # payments_statistics = pd.DataFrame({'Payment Type':[Utils.PAYMENTS_TYPE_DICTIONARY.get(index) for index in payments_type.index], 'Count':payments_type.values})
     # print(payments_statistics.hist(bins=6))
